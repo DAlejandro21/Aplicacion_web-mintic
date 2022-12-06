0e5fa56126822806dd06cb5a6f0bf1bafcbdad29
DAlejandro21 <david.acosta.21@outlook.es>
2022-12-06 09:22:06 -0500

    tabla para editar y eliminar productos

--- cliente/src/ListaProdutos.js
+++ cliente/src/ListaProdutos.js
@@ -1,25 +1,50 @@
 import axios from "axios";
 import { set } from "mongoose";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import VerProducto from "./VerProducto";
 
 function ListaProductos() {
+  const [dataproductos, setdataproductos] = useState([]);
 
-  const[dataproductos, setdataproductos]=useState([])
+  useEffect(() => {
+    axios
+      .get("/api/producto/obtenerproductos")
+      .then((res) => {
+        console.log(res.data);
+        setdataproductos(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-  useEffect(()=> {
-    axios.get('/api/producto/obtenerproductos'). then(res => {
-      console.log(res)
-    }). catch(err =>{
-      console.log(err)
-    })
-  })
-  
+  //Mapear los datos de los productos
+  const listaproductos = dataproductos.map((producto) => {
+    return (
+      <div className="container">
+        <VerProducto producto={producto} />
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h1>Ver lista de productos</h1>
-      <VerProducto/>
+    <div className="container">
+      <h2>Ver lista de productos</h2>
+      <table className="table">
+        <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nombre de Producto</th>
+              <th>Unidad M.</th>
+              <th>Categoria</th>
+              <th>Descripcion</th>
+              <td>-</td>
+            </tr>
+        </thead>
+        </table>
+      {listaproductos}
     </div>
-  )
+  );
 }
+
 export default ListaProductos;
--- cliente/src/VerProducto.js
+++ cliente/src/VerProducto.js
@@ -1,10 +1,28 @@
-import React from "react"
+import React from "react";
+import {Link} from 'react-router-dom'
 
-function VerProducto() {
+function VerProducto({ producto }) {
   return (
-    <div>
-      <h1>Ver productos</h1>
+    <div className="container">
+      <table className="table">
+      <tbody>
+        <tr>
+          <td>{producto.idProducto}</td>
+          <td>{producto.nombreProducto}</td>
+          <td>{producto.unidadMed}</td>
+          <td>{producto.categoria}</td>
+          <td>{producto.descripcion}</td>
+          <td>
+            <Link to={'/editarproducto/${producto.idProducto}'}>
+              <li class="btn btn-primary">Editar</li>
+            </Link>
+            &nbsp;
+            <button type="button" class="btn btn-danger">Eliminar</button>
+          </td>
+        </tr>
+      </tbody>
+      </table>
     </div>
-  )
+  );
 }
-export default VerProducto
+export default VerProducto;
